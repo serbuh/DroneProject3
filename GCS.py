384f9e3ce98b25a54aa9b7011e2b38278c34153e
Sergey Buh <ser.buh@gmail.com>
2016-05-10 14:30:57 -0700

    GUI as a class

--- GCS.py
+++ GCS.py
@@ -1,12 +1,13 @@
 import threading
 import time
 from GCS_UDP_client import *
+from GCS_GUI import *
 
 HOST = 'localhost'
 TELEM_PORT = 3334
 VIDEO_PORT = 3333
 
-val_dict = dict.fromkeys(['roll', 'pitch', 'yaw', 'vx', 'vy', 'vz', 'heading', 'rangefinder', 'lat', 'lon', 'alt', 'airspeed', 'groundspeed', 'gimbal_roll', 'gimbal_pitch', 'gimbal_yaw', 'last_heartbeat'])
+val_dict = dict.fromkeys(['roll', 'pitch', 'yaw', 'vx', 'vy', 'vz', 'heading', 'rangefinder', 'airspeed', 'groundspeed', 'gimbal_roll', 'gimbal_pitch', 'gimbal_yaw', 'lat_loc', 'lon_loc', 'alt_loc', 'lat_gl', 'lon_gl', 'alt_gl', 'lat_gl_rel', 'lon_gl_rel', 'alt_gl_rel', 'last_heartbeat'])
 
 def get_telem(telem_client,run_event):
 	global val_dict	
@@ -15,7 +16,22 @@ def get_telem(telem_client,run_event):
 		print data_json
 		#TODO put in val_dict
 
+def inf_loop():
+	while 1:
+		time.sleep(1)
+
+def close_all(run_event,get_telem,telem_client):
+	print "Send event to all running threads to close"
+	run_event.clear()
+	get_telem_thread.join()
+	print "get_telem_thread dead"
+	telem_client.close_client()
+	print "Socket closed"
+	print "######## My work here is DONE ########"
+
 if __name__ == "__main__":
+	'''
+	print "*** START TELEM ***"	
 	telem_client = GCS_UDP_client()
 	telem_client.connect(HOST,TELEM_PORT)
 
@@ -24,17 +40,14 @@ if __name__ == "__main__":
 	
 	print("Start get_telem_thread")
 	get_telem_thread = threading.Thread(target=get_telem, args=[telem_client,run_event])
-	get_telem_thread.start()
+	get_telem_thread.start()'''
+	
+	print "*** START GUI ***"
+	GUI=GCS_GUI(val_dict)
 
 	try:
-		while 1:
-			time.sleep(1)
+		inf_loop()
 	except(KeyboardInterrupt , SystemExit):
-		print "Send event to all running threads to close"
-		run_event.clear()
-		get_telem_thread.join()
-		print "get_telem_thread dead"
-		telem_client.close_client()
-		print "Socket closed"
-		print "######## My work here is DONE ########"
+		#close_all(run_event,get_telem,telem_client)
+		pass
 
