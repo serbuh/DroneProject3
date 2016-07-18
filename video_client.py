18788d77baf375af496a26231714932473fed40e
shedows89 <shedows89@gmail.com>
2016-07-18 07:23:33 -0700

    clean video_client.py file

--- video_client.py
+++ video_client.py
@@ -28,23 +28,14 @@ def reciveAndQueue(queue,socket,run_event):
 		packet = socket.recv(60000)
     	print "Recive1!"
     	data = eval(packet)
-    	#print "Queue!"
     	queue.put(data)
 
 def readAndShow(queue,run_event):
 	full_data = ''
 	while run_event.is_set():
 		try:
-			#data = queue.get()
-			#print type(eval(data))
-					#print "Show!"
 			data = queue.get()
-					#data = eval(data)
-					#print(type(data[1]))
-					#print data[1]
-					#print(type(data[1]))
 			full_data = ''.join([full_data,data])
-					#print len(full_data)
 			if(len(full_data) == 921600):
 				tmp_frame = numpy.fromstring(full_data, dtype=numpy.uint8)
 				frame = numpy.reshape(tmp_frame, (480,640,3))
@@ -63,17 +54,12 @@ if __name__ == "__main__":
 	run_event = threading.Event()
 	run_event.set() 
 
-	'''reciveThread = threading.Thread(target=reciveAndQueue, args=[q,s,run_event])
-	print "Start Recive Thread!"
-	reciveThread.start()
-	time.sleep(1)'''
 	showThread = threading.Thread(target=readAndShow, args=[q,run_event])
 	print "Start Show Thread!"
 	showThread.start()
 
 	flag = 0
 	while True:
-		#print "Main Loop"
 		try:
 			data = s.recv(60000)
 			data = eval(data)
@@ -83,9 +69,7 @@ if __name__ == "__main__":
 					data = s.recv(60000)
 					data = eval(data)
 					q.put(data[1])
-			#print eval(data)[0]
  		except(KeyboardInterrupt , SystemExit):
 			run_event.clear()
-			#reciveThread.join()
 			showThread.join()
 			s.close()
