85f8be4544e60a93e7f05fa094b8a4d6a3a36276
shedows89 <shedows89@gmail.com>
2016-08-09 11:52:42 -0700

    More stable version of video over udp

--- video_client.py
+++ video_client.py
@@ -5,12 +5,13 @@ import copy
 from  Queue import Queue
 import threading
 import time
+import errno
+from time import sleep
 
-
-HOST = '192.168.12.1'
+HOST = '127.0.0.1'
 PORT = 3333
 
-def showImage(title , frame ,event, wait = False ):	
+def showImage(title , frame , run_event, wait = False ):	
 	cv2.imshow(title, frame)
     	if wait:
         	while True:
@@ -18,8 +19,8 @@ def showImage(title , frame ,event, wait = False ):
               			break
         	cv2.destroyAllWindows()
     	else:
-        	if cv2.waitKey(1)&0xff == ord('q'):
-            		event.clear()
+			if cv2.waitKey(1)&0xff == ord('q'):
+				raise KeyboardInterrupt
 
 #def reciveAndQueue(queue,socket,run_event):
 #	flag = 0
@@ -32,48 +33,64 @@ def showImage(title , frame ,event, wait = False ):
 
 def recieveAndQueue(queue,run_event):
 	while run_event.is_set():
-		data = s.recv(60000)
-		data = eval(data)
-		queue.put(data)
+		try:
+			data = s.recv(60000)
+			#print "Got packet!"
+			data = eval(data)
+			queue.put(data)
+		except(KeyboardInterrupt):
+			print "Thread interupt..."
+			close_event.set()
+		except socket.timeout, e:
+	            sleep(1)
+	            print 'recv timed out, retry later'
+	            continue
 	print "Recieve Thread Closed"
 
 if __name__ == "__main__":
-	
+	i = 0
 	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 	s.bind((HOST,PORT))
-	s.settimeout(0.1)
+	s.settimeout(1)
 	q = Queue()
 	run_event = threading.Event()
+	close_event = threading.Event()
 	run_event.set() 
 
 	recieveThread = threading.Thread(target=recieveAndQueue, args=[q,run_event])
 	print "Start Recieve Thread!"
 	recieveThread.start()
 	full_data = ''
-	while run_event.is_set():
+	last_packet_num = -1
+	while not close_event.is_set():
 		try:
-			data = q.get()
-			#print data[0]
-			#if len(full_data) != 0 and data[0] == 0:
-			#	full_data = ''
-			#	print "Reset frame!"
-			#else:
-			if data[0] == 0 :
-				full_data = data[1]
-			else :
-				full_data = ''.join([full_data,data[1]])
-			#print len(full_data),data[0]
-			if(len(full_data) == 921600):
-				tmp_frame = numpy.fromstring(full_data, dtype=numpy.uint8)
-				frame = numpy.reshape(tmp_frame, (480,640,3))
-				showImage("Client", frame)
-				full_data = ''
-			if cv2.waitKey(1)&0xff == ord('q'):
-				raise KeyboardInterrupt
-			
- 		except(KeyboardInterrupt , SystemExit):
+			if not q.empty():
+				data = q.get()
+				print data[0]
+				if data[0] == 0:
+					full_data = data[1]
+					last_packet_num = 0
+					print "Reset frame!"
+				elif data[0] == last_packet_num + 1:
+					full_data = ''.join([full_data,data[1]])
+				#print len(full_data),data[0]
+				if(len(full_data) == 921600):
+					#print "SHOWING FRAME: {}".format(i)
+					#i = i +1
+					tmp_frame = numpy.fromstring(full_data, dtype=numpy.uint8)
+					frame = numpy.reshape(tmp_frame, (480,640,3))
+					showImage("Client", frame,run_event)
+					full_data = ''
+				
+ 		except(KeyboardInterrupt):
+			print "Main loop interupt..."
 			run_event.clear()
+			print "Joining..."
 			recieveThread.join()
-			s.close()
+			print "Dead..."
 			cv2.destroyAllWindows()
 			break
+
+	if close_event.is_set():
+		cv2.destroyAllWindows()
+
